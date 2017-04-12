a38f0ab4ad870c9a04f3e44ae37fccf3558620f0
Jade Faist <jade@Jades-MacBook-Pro.local>
2017-04-12 11:19:27 -0700

    react working

--- source/components/App.js
+++ source/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { version } from '../../package.json';
 
 const App = ({ children }) => (
--- source/index.js
+++ source/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const hashHistory = createBrowserHistory();
+
+// ----
 import App from './components/app';
-import PoweredBy from './components/powered-by';
 import About from './components/about';
+import PoweredBy from './components/powered-by';
 
 window.React = React;
 
@@ -13,5 +18,5 @@ render(
       <Route path="/about" component={About} />
       <Route path="/poweredby" component={PoweredBy} />
     </Route>
-  </Router>), document.getElementById('app')
+  </Router>), document.getElementById('root')
 );
