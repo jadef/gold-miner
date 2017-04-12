88a838e28f1cf1652c8d9ab4d454e89dd927dc72
Jade Faist <jade@Jades-MacBook-Pro.local>
2017-04-12 13:44:09 -0700

    remove history for now

--- source/index.js
+++ source/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { Router, Route } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory'
-
-const hashHistory = createBrowserHistory();
-
-// ----
-import App from './components/app';
-
 window.React = React;
 
+import App from './components/App';
+
 render(
-  (<Router history={hashHistory}>
-    <Route path="/" component={App}>
-    </Route>
-  </Router>), document.getElementById('root')
+  <App />,
+  document.getElementById('root')
 );
