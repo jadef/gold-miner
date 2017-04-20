cc638c06225586eae58fe79c22cd1d7687f0427a
jadef <jade@jfaist.com>
2017-04-20 12:33:59 -0700

    BuildEntries to prep for filtering

--- source/js/sections/Entries.jsx
+++ source/js/sections/Entries.jsx
@@ -3,28 +3,59 @@ import React from 'react';
 // -- Components
 import Entry from '../components/entries/Entry';
 
+/*
+- state array of entry IDs visible
+- update state from tags child component events
+  - hide IDs that aren't in array (unmount?)
+  - active control tag
+  - active entry tag
+*/
+
 // -- Data
-import { entries  } from '../data/entries.json'
+import { entries } from '../data/entries.json'
+
+
 
 class Entries extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeEntries: entries
+    };
+  }
 
-    // Alphabetical sort of id
-    const sortEntries = entries.sort(function(a, b) {
-      return a.id.localeCompare(b.id);
+  handleClick = () => {
+    this.setState({
+      activeEntries: entries
     });
 
-    // Fetch all entries
-    const allEntries = sortEntries.map((data, i) => (
-      <Entry entry={data} key={i} />
-    ));
+    // console.log(this.state.activeEntries);
+    this.BuildEntries();
+  }
 
-    // Return wrapped entries
+  BuildEntries = (props) => {
+
+    // Alphabetical sort of id
+    const sortEntries = this.state.activeEntries.sort(function(a, b) {
+      return a.id.localeCompare(b.id);
+    })
+
+    const allEntries = sortEntries.map((data) => (
+      <Entry entry={data} key={data.id} />
+    ))
+
+    console.log( allEntries );
+    return ( allEntries );
+
+  }
+
+  render() {
     return (
       <div className="entries">
+        <button onClick={this.handleClick}>Ping</button>
         <div className="fade" />
         <br className="jump" id="jumpA" />
-        {allEntries}
+        {this.BuildEntries()}
       </div>
     );
   }
