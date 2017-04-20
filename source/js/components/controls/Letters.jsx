d877683fe37c8eb5c20e2efc9e79ceaa31fef1a1
jadef <jade@jfaist.com>
2017-04-20 15:10:57 -0700

    WIP letters active state

--- source/js/components/controls/Letters.jsx
+++ source/js/components/controls/Letters.jsx
@@ -13,11 +13,40 @@ TODO:
 // -- Data
 import { letters } from '../../data/app.json'
 
-function Letter (props) {
-  let classes = "";
-  (props.disabled ? classes += "none" : null);
-  (props.first == 0 ? classes += " active" : null);
-  return <li className={classes}><a href={"#jump" + props.letter}>{props.letter}</a></li>;
+class Letter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    };
+  }
+
+  handleClick = () => {
+    /*
+    - remove all active classes
+    - add active class
+    - placehold MORE
+    */
+
+    const currentState = this.state.active;
+    this.setState({
+      active: !currentState
+    });
+  }
+
+  render() {
+    let classes = "";
+    (this.props.disabled ? classes += "none" : null);
+    (this.props.first == 0 ? classes += " active" : null);
+    (this.state.active && !this.props.disabled ? classes += " active" : null);
+
+    return (
+      <li
+        className={classes}
+        onClick={this.handleClick}
+      ><a href={"#jump" + this.props.letter}>{this.props.letter}</a></li>
+    );
+  }
 }
 
 class ControlsLetters extends React.Component {
@@ -29,24 +58,26 @@ class ControlsLetters extends React.Component {
     ));
 
     // Return All Letters (and handle)
-    return <section className="letters controls">
-      <div className="handle">
-        <svg viewBox="0 0 32 32">
-          <defs>
-            <filter id="triangle-shadow" x="-55.0%" y="-35.0%" width="210.0%" height="210.0%" filterUnits="objectBoundingBox">
-              <feOffset dx="0" dy="4" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
-              <feGaussianBlur stdDeviation="3" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
-              <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.15 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
-            </filter>
-          </defs>
-          <g transform="translate(10, 10) rotate(-90) translate(-10, -10) ">
-            <use fill="black" fillOpacity="1" filter="url(#triangle-shadow)" xlinkHref="#triangle"></use>
-            <polygon id="triangle" points="10 0 20 20 0 20"></polygon>
-          </g>
-        </svg>
-      </div>
-      <ul>{allLetters}</ul>
-    </section>;
+    return (
+      <section className="letters controls">
+        <div className="handle">
+          <svg viewBox="0 0 32 32">
+            <defs>
+              <filter id="triangle-shadow" x="-55.0%" y="-35.0%" width="210.0%" height="210.0%" filterUnits="objectBoundingBox">
+                <feOffset dx="0" dy="4" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+                <feGaussianBlur stdDeviation="3" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+                <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.15 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
+              </filter>
+            </defs>
+            <g transform="translate(10, 10) rotate(-90) translate(-10, -10) ">
+              <use fill="black" fillOpacity="1" filter="url(#triangle-shadow)" xlinkHref="#triangle"></use>
+              <polygon id="triangle" points="10 0 20 20 0 20"></polygon>
+            </g>
+          </svg>
+        </div>
+        <ul>{allLetters}</ul>
+      </section>
+    );
   }
 }
 
