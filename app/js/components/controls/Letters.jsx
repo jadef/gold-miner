095f0f884903203eed9924b5b912638af125e082
jadef <jade@jfaist.com>
2017-05-17 16:27:22 -0700

    nearly functional handle moving

--- app/js/components/controls/Letters.jsx
+++ app/js/components/controls/Letters.jsx
@@ -53,22 +53,24 @@ class ControlsLetters extends React.Component {
     // Return All Letters (and handle)
     return (
       <section className="letters controls">
-        <div className="handle">
-          <svg viewBox="0 0 32 32">
-            <defs>
-              <filter id="triangle-shadow" x="-55.0%" y="-35.0%" width="210.0%" height="210.0%" filterUnits="objectBoundingBox">
-                <feOffset dx="0" dy="4" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
-                <feGaussianBlur stdDeviation="3" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
-                <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.15 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
-              </filter>
-            </defs>
-            <g transform="translate(10, 10) rotate(-90) translate(-10, -10) ">
-              <use className="c-bg-dark" fillOpacity="1" filter="url(#triangle-shadow)" xlinkHref="#triangle"></use>
-              <polygon id="triangle" className="c-bg-primary" points="10 0 20 20 0 20"></polygon>
-            </g>
-          </svg>
+        <div className="letter-wrapper">
+          <ul>{allLetters}</ul>
+          <div id="letter-handle" className="handle">
+            <svg viewBox="0 0 32 32">
+              <defs>
+                <filter id="triangle-shadow" x="-55.0%" y="-35.0%" width="210.0%" height="210.0%" filterUnits="objectBoundingBox">
+                  <feOffset dx="0" dy="4" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+                  <feGaussianBlur stdDeviation="3" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+                  <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.15 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
+                </filter>
+              </defs>
+              <g transform="translate(10, 10) rotate(-90) translate(-10, -10) ">
+                <use className="c-bg-dark" fillOpacity="1" filter="url(#triangle-shadow)" xlinkHref="#triangle"></use>
+                <polygon id="triangle" className="c-bg-primary" points="10 0 20 20 0 20"></polygon>
+              </g>
+            </svg>
+          </div>
         </div>
-        <ul>{allLetters}</ul>
       </section>
     );
   }
@@ -78,7 +80,16 @@ class ControlsLetters extends React.Component {
 class Letter extends React.Component {
 
   handleClick(event) {
-    this.props.disabled ? event.preventDefault() : this.props.onClick(this.props.index);
+    if (!this.props.disabled) {
+      // Make only this active (passed to parent component)
+      this.props.onClick(this.props.index);
+      // Compute math for Handle Position
+      let topLocation = this.props.index / 26 * 100;
+      // Find Handle element and add top style
+      document.getElementById("letter-handle").style.top = topLocation + "%";
+    } else {
+      event.preventDefault();
+    }
   }
 
   render () {
