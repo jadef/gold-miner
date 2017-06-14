19e0c407d965575ef991b2306ef465b7055d1673
jadef <jade@jfaist.com>
2017-06-14 16:28:39 -0700

    Empty jest tests to get started

---
+++ __tests__/controls/letters.test.js
@@ -0,0 +1,12 @@
+// Link to component
+const Letters = require('../../app/js/components/controls/Letters');
+
+// Describe
+test('ControlsLetters', () => {
+  // expect().toBe()
+});
+
+
+test('Letter', () => {
+
+});
---
+++ __tests__/controls/tags.test.js
@@ -0,0 +1,12 @@
+// Link to component
+const tags = require('../../app/js/components/controls/Tags');
+
+// Describe
+test('ControlsTags', () => {
+  // expect().toBe()
+});
+
+
+test('Tag', () => {
+
+});
