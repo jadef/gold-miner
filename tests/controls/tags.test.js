d315d1e8bdd2334af66d522c407d176c5308e398
jadef <jade@jfaist.com>
2017-06-22 12:15:46 -0700

    add first snapshot test for tags

--- tests/controls/tags.test.js
+++ tests/controls/tags.test.js
@@ -1,12 +1,24 @@
-// Link to component
-const tags = require('../../app/js/components/controls/Tags');
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+// Link to component(s)
+import ControlsTags from '../../app/js/components/controls/Tags';
+import Tag from '../../app/js/components/controls/Tags';
 
 // Describe
-test('ControlsTags', () => {
-  // expect().toBe()
+test('ControlsTags Renders', () => {
+  const component = renderer.create(
+    <ControlsTags />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
 
-test('Tag', () => {
-
+test('Tag Renders', () => {
+  const component = renderer.create(
+    <Tag />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
