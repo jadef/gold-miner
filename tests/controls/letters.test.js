31672036b2dbb9d313893c626528fe23a84b0494
jadef <jade@jfaist.com>
2017-06-23 10:52:01 -0700

    letters snapshot test

--- tests/controls/letters.test.js
+++ tests/controls/letters.test.js
@@ -1,12 +1,24 @@
-// Link to component
-const Letters = require('../../app/js/components/controls/Letters');
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+// Link to component(s)
+import ControlsLetters from '../../app/js/components/controls/Letters';
+import Letter from '../../app/js/components/controls/Letters';
 
 // Describe
-test('ControlsLetters', () => {
-  // expect().toBe()
+test('ControlsLetters Renders', () => {
+  const component = renderer.create(
+    <ControlsLetters />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
 
-test('Letter', () => {
-
+test('Letter Renders', () => {
+  const component = renderer.create(
+    <Letter />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
